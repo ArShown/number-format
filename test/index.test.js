21301126d1e6abd611521abd2f419285f4803aae
ArShown <arshown@megalive.com.tw>
2019-10-08 17:08:18 +0800

    [add] 新增套件單元測試-整數

--- test/index.test.js
+++ test/index.test.js
@@ -1,1 +1,44 @@
+import { expect } from 'chai';
+import number_format from '../source/index';
 
+describe('number_format', () => {
+  describe('整數', () => {
+    it("(123456.789, '0') => '123456'", () => {
+      expect(number_format(123456.789, '0')).to.be.eql('123456');
+    });
+    it("(123456.789, '3') => '123456'", () => {
+      expect(number_format(123456.789, '3')).to.be.eql('123456');
+    });
+    it("(123456.789, '7') => '0123456'", () => {
+      expect(number_format(123456.789, '7')).to.be.eql('0123456');
+    });
+    it("(123456.789, '-3') => '123456'", () => {
+      expect(number_format(123456.789, '-3')).to.be.eql('456');
+    });
+    it("(123456.789, '') => ''", () => {
+      expect(number_format(123456.789, '')).to.be.eql('');
+    });
+  });
+  describe('小數', () => {
+    it("(123456.789, '.2') => '.78'", () => {
+      expect(number_format(123456.789, '.2')).to.be.eql('.78');
+    });
+    it("(123456.789, '.4') => '.78'", () => {
+      expect(number_format(123456.789, '.4')).to.be.eql('.7800');
+    });
+    it("(123.456, '2.2') => '123.45'", () => {
+      expect(number_format(123.456, '2.2')).to.be.eql('123.45');
+    });
+    it("(123.456, '4.4') => '0123.4560'", () => {
+      expect(number_format(123.456, '4.4')).to.be.eql('0123.4560');
+    });
+  });
+
+  describe('建構式方式', () => {
+    it("(123456.789, '0') => '123456'", () => {
+      const number = new number_format(123456.789, '0');
+      expect(number.value()).to.be.eql('123456');
+      expect(number.format('0.4')).to.be.eql('123456.7890');
+    });
+  });
+});
