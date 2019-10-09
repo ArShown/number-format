819dab16463942e59e3cd09afe0eb5693fd9009b
ArShown <arshown@megalive.com.tw>
2019-10-09 10:29:04 +0800

    [edit] match-group

--- source/utils/match-group.js
+++ source/utils/match-group.js
@@ -1,5 +1,96 @@
-import { map, pipe, match, drop, dropLast } from 'ramda';
+import {
+  forEach,
+  addIndex,
+  split,
+  append,
+  slice,
+  findLastIndex,
+  findIndex,
+  tail,
+  head,
+  remove,
+  last,
+  defaultTo,
+  init,
+  reduce,
+  join,
+  includes
+} from 'ramda';
 
-const matchGroup = pipe(match(/\(.+?\)/g), map(pipe(drop(1), dropLast(1))));
+/**
+ * 取得所在群組
+ * @private
+ * @param {Array<[number,number]>} group 群組序列號
+ * @param {number} idx 序列號
+ * @return {number} group的序列號：-1 表示不在任何群組中
+ */
+const _inGroupIndex = group => idx => findIndex(([startIdx, endIdx]) => idx >= startIdx && idx <= endIdx)(group);
+
+const matchGroup = formatStr => {
+  /* 初始化 */
+  let group = [],
+    leftBrackets = [],
+    rightBrackets = [],
+    slitFormatStr = split('', formatStr);
+
+  /* 統計字元 */
+  addIndex(forEach)((str, idx) => {
+    switch (str) {
+      case '(':
+        leftBrackets = append(idx, leftBrackets);
+        break;
+      case ')':
+        if (leftBrackets.length === (rightBrackets.length + 1))
+          rightBrackets = append(idx, rightBrackets);
+        else
+          leftBrackets = init(leftBrackets);
+        break;
+    }
+  }, slitFormatStr);
+
+  /* 例外處理 */
+  if (leftBrackets.length !== rightBrackets.length) throw '格式錯誤';
+
+  /* 包裝分類 */
+  while (rightBrackets.length > 0) {
+    const [_, lastRight] = defaultTo(['', -1], last(group));
+    const nextRight = head(rightBrackets);
+    const leftIdx = findLastIndex(idx => idx < nextRight)(leftBrackets);
+    const nextLeft = leftBrackets[leftIdx];
+    group = append([nextLeft, nextRight])(group);
+    leftBrackets = remove(leftIdx, 1)(leftBrackets);
+    rightBrackets = tail(rightBrackets);
+  }
+
+  /**
+   *  組裝輸出的字串
+   *  群組的部分要變成變數格式
+   *  ex. aaa(bbb)ccc => aaa$1ccc
+   */
+  const getGroupIndex = _inGroupIndex(group);
+  const origin = addIndex(reduce)((combineStr, unitStr, idx) => {
+    /* 判斷字元是不是群組中 */
+    const groupIdx = getGroupIndex(idx);
+    if (groupIdx === -1)
+      combineStr = append(unitStr, combineStr);
+    else {
+      /* 群組是不是已加入 */
+      if (!includes(`$${groupIdx + 1}`, combineStr))
+        combineStr = append(`$${groupIdx + 1}`, combineStr);
+    }
+    return combineStr;
+  }, [], slitFormatStr);
+
+  /* 群組字元對應表 */
+  const objChild = addIndex(reduce)((obj, [startIdx, endIdx], idx) => {
+    obj[`$${idx + 1}`] = slice(startIdx + 1, endIdx, formatStr);
+    return obj;
+  }, {}, group);
+
+  return {
+    origin: join('', origin),
+    ...objChild
+  };
+};
 
 export default matchGroup;
--- test/utils/match-group.test.js
+++ test/utils/match-group.test.js
@@ -3,11 +3,35 @@ import matchGroup from '../../source/utils/match-group';
 
 describe('matchGroup', () => {
   it('解析群組', () => {
-    expect(matchGroup('(1)(2)(3)')).to.be.eql([
-      '1', '2', '3'
-    ]);
-    expect(matchGroup('(abc)aa(def)bb(ghi)')).to.be.eql([
-      'abc', 'def', 'ghi'
-    ]);
+    expect(matchGroup('(1)(2)(3)')).to.be.eql({
+      origin: '$1$2$3',
+      $1: '1',
+      $2: '2',
+      $3: '3'
+    });
+    expect(matchGroup('(abc)aa(def)bb(ghi)cc')).to.be.eql({
+      origin: '$1aa$2bb$3cc',
+      $1: 'abc',
+      $2: 'def',
+      $3: 'ghi'
+    });
+    expect(matchGroup('(aaa(bbb(ccc))ddd)eee')).to.be.eql({
+      origin: '$1eee',
+      $1: 'aaa(bbb(ccc))ddd'
+    });
+    expect(matchGroup('aaa(bbb(ccc))ddd')).to.be.eql({
+      origin: 'aaa$1ddd',
+      $1: 'bbb(ccc)'
+    });
+    expect(matchGroup('bbb(ccc)')).to.be.eql({
+      origin: 'bbb$1',
+      $1: 'ccc'
+    });
+  });
+
+  it('錯誤處理', () => {
+    expect(matchGroup, '(((aaa)').to.throw();
+    expect(matchGroup, '(((aaa)))))').to.throw();
+    expect(matchGroup, '(aaa(bbb(ccc))ddd)eee)').to.throw();
   });
 });
